99aa4f975247f00f8cf8ad8c39e13a39b2cbc886
jumahi <mrjasonu@gmail.com>
2022-10-30 01:29:51 +0100

    sixth commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -63,15 +63,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-  <h1>Ja<span className="highlight">mmm</span>ing by Jason</h1>
-  <div className="App">
-    <SearchBar onSearch={this.search} />
-    <div className="App-playlist">
-      <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-      <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
-    </div>
-  </div>
-</div>
+        <h1>Ja<span className="highlight">mmm</span>ing by Jason</h1>
+        <div className="App">
+          <SearchBar onSearch={this.search} />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
+          </div>
+        </div>
+      </div>
     )
   }
 }
